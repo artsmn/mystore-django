21ec67ca18fe76f1d35e4a4e1eb654b7d0ca45ec
Artem Oliinyk <artem.oleynik.main@gmail.com>
2020-06-18 11:07:04 +0300

    update static\media path

--- mystore/settings.py
+++ mystore/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/artsmn/dev/mystore/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-MEDIA_ROOT = '/home/artsmn/dev/mystore/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 MEDIA_URL = '/media/'
 
